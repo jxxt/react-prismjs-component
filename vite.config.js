5047a94119a4724a768d6094b2138adb4de7af5b
Jeet Debnath <jeet.debnath2004@gmail.com>
2025-01-04 18:38:22 +0530

    updated scripts

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,31 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     build: {
         lib: {
-            entry: "./src/index.js", 
+            entry: "./src/index.js",
             name: "ReactPrismJSComponent",
             fileName: (format) => `react-prismjs-component.${format}.js`,
         },
         rollupOptions: {
-            external: ["react", "react-dom"], 
+            external: ["react", "react-dom"],
             output: {
                 globals: {
                     react: "React",
                     "react-dom": "ReactDOM",
                 },
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name.endsWith('.css')) {
+                        return 'styles/[name][extname]';
+                    }
+                    return 'assets/[name][extname]';
+                },
             },
         },
     },
     plugins: [react()],
+    css: {
+        modules: {
+            localsConvention: 'camelCase',
+            generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+    }
 });
